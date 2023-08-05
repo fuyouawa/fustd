b4761ab4553937b2cbecc439941df14294dc0222
Noithw <2907142541@qq.com>
2023-08-05 22:09:18 +0800

    增强了HashList

--- hash_list.hpp
+++ hash_list.hpp
@@ -32,46 +32,44 @@ struct DefaultTypeInfoGetter {
 };
 }
 
-template<class T, class TypeInfoGetter=details::DefaultTypeInfoGetter>
+template<class TypeInfoGetter=details::DefaultTypeInfoGetter>
 class HashList
 {
 public:
 	using TDeleter = std::function<void(void*)>;
-	using Container = std::unordered_map<details::TypeInfoWrapper, std::pair<T*, TDeleter>, details::TypeInfoWrapperHasher, details::TypeInfoWrapperEqualTo>;
+	using Container = std::unordered_map<details::TypeInfoWrapper, std::pair<void*, TDeleter>, details::TypeInfoWrapperHasher, details::TypeInfoWrapperEqualTo>;
 	using ContainerIterator = typename Container::iterator;
-	template<class U>
-	using EnableIfConvertibleT = std::enable_if_t<std::is_convertible_v<U*, T*>, int>;
 
-	class Iterator {
-	public:
-		using iterator_category = std::forward_iterator_tag;
-		using value_type = T;
-		using difference_type = std::ptrdiff_t;
-		using pointer = T*;
-		using reference = T&;
+	//class Iterator {
+	//public:
+	//	using iterator_category = std::forward_iterator_tag;
+	//	using value_type = T;
+	//	using difference_type = std::ptrdiff_t;
+	//	using pointer = T*;
+	//	using reference = T&;
 
-		reference operator*() const { return (*iter_).second.first; }
-		pointer operator->() const { return &(operator*()); }
-		Iterator& operator++() {
-			iter_++;
-			return *this;
-		}
-		Iterator operator++(int) {
-			Iterator tmp = *this;
-			++*this;
-			return tmp;
-		}
-		friend bool operator==(const Iterator& a, const Iterator& b) const noexcept { return a.iter_ == b.iter_; }
-		friend bool operator!=(const Iterator& a, const Iterator& b) const noexcept { return a.iter_ != b.iter_; }
+	//	reference operator*() const { return (*iter_).second.first; }
+	//	pointer operator->() const { return &(operator*()); }
+	//	Iterator& operator++() {
+	//		iter_++;
+	//		return *this;
+	//	}
+	//	Iterator operator++(int) {
+	//		Iterator tmp = *this;
+	//		++*this;
+	//		return tmp;
+	//	}
+	//	friend bool operator==(const Iterator& a, const Iterator& b) const noexcept { return a.iter_ == b.iter_; }
+	//	friend bool operator!=(const Iterator& a, const Iterator& b) const noexcept { return a.iter_ != b.iter_; }
 
-	private:
-		friend class HashList;
-		Iterator() {}
-		Iterator(const ContainerIterator& iter) {
-			iter_ = iter;
-		}
-		ContainerIterator iter_;
-	};
+	//private:
+	//	friend class HashList;
+	//	Iterator() {}
+	//	Iterator(const ContainerIterator& iter) {
+	//		iter_ = iter;
+	//	}
+	//	ContainerIterator iter_;
+	//};
 
 	HashList(): typeinfo_getter_(), container_() {}
 	template<class... Types>
@@ -84,18 +82,18 @@ public:
 		}
 	}
 
-	template<class U, EnableIfConvertibleT<U> = 0>
-	void Insert(U* val) {
-		auto found = container_.find(GetType<U>());
+	template<class T>
+	void Insert(T* val) {
+		auto found = container_.find(GetType<T>());
 		if (found != container_.end()) {
 			DeleteValue(found->second);
 		}
-		container_[GetType<U>()] = { dynamic_cast<T*>(val), [](void* ptr) { delete static_cast<U*>(ptr); } };
+		container_[GetType<T>()] = { val, [](void* ptr) { delete static_cast<T*>(ptr); } };
 	}
 
-	template<class U, EnableIfConvertibleT<U> = 0>
+	template<class T>
 	bool Erase() {
-		auto found = container_.find(GetType<U>());
+		auto found = container_.find(GetType<T>());
 		if (found != container_.end()) {
 			container_.erase(found);
 			return true;
@@ -103,14 +101,14 @@ public:
 		return false;
 	}
 
-	template<class U, EnableIfConvertibleT<U> = 0>
-	Iterator Find() const noexcept {
-		return Iterator(container_.find(GetType<U>()));
-	}
+	//template<class T>
+	//Iterator Find() const noexcept {
+	//	return Iterator(container_.find(GetType<T>()));
+	//}
 
-	template<class U, EnableIfConvertibleT<U> = 0>
+	template<class T>
 	bool Contain() const noexcept {
-		return container_.find(GetType<U>()) != container_.end();
+		return container_.find(GetType<T>()) != container_.end();
 	}
 
 	bool Empty() const noexcept {
@@ -125,27 +123,27 @@ public:
 		container_.clear();
 	}
 
-	template<class U, EnableIfConvertibleT<U> = 0>
-	fystd::Option<U> GetValue() const noexcept {
-		auto found = container_.find(GetType<U>());
+	template<class T>
+	fystd::Option<T> GetValue() const noexcept {
+		auto found = container_.find(GetType<T>());
 		if (found)
 			return fystd::Some(found.second.first);
 		return fystd::None;
 	}
 	
-	Iterator begin() { return Iterator(container_.begin()); }
-	Iterator end() { return Iterator(container_.end()); }
+	//Iterator begin() { return Iterator(container_.begin()); }
+	//Iterator end() { return Iterator(container_.end()); }
 
-	Iterator begin() const { return Iterator(container_.begin()); }
-	Iterator end() const { return Iterator(container_.end()); }
+	//Iterator begin() const { return Iterator(container_.begin()); }
+	//Iterator end() const { return Iterator(container_.end()); }
 
 private:
-	template<class U>
+	template<class T>
 	const std::type_info& GetType() const noexcept {
-		return typeinfo_getter_((U*)0);
+		return typeinfo_getter_((T*)0);
 	}
 
-	void DeleteValue(std::pair<T*, TDeleter>& x) {
+	void DeleteValue(std::pair<void*, TDeleter>& x) {
 		if (x.first) {
 			x.second(x.first);
 		}
