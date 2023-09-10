330d7e11a8fa27664845e9588155c962418a035a
Noithw <2907142541@qq.com>
2023-09-10 10:46:18 +0800

    新增了algorithm.h

--- generic/algorithm.hpp
+++ generic/algorithm.hpp
@@ -3,13 +3,13 @@
 #include <fustd/generic/type_traits.hpp>
 
 namespace fustd {
-template<class T, std::enable_if_t<std::is_integral_v<T>, int> = 0>
-constexpr T FloorDiv(T dividend, T divisor) {
+template<class T, class E, std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<E>, int> = 0>
+constexpr T FloorDiv(T dividend, E divisor) {
 	return dividend / divisor;
 }
 
-template<class T, std::enable_if_t<std::is_integral_v<T>, int> = 0>
-constexpr T CeilDiv(T dividend, T divisor) {
+template<class T, class E, std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<E>, int> = 0>
+constexpr T CeilDiv(T dividend, E divisor) {
 	T res = dividend / divisor;
 	return (dividend % divisor) != 0 ? res + 1 : res;
 }
