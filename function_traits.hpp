55c763eb7a065ae14d8eea9abfc0d90e4a09f571
Noithw <2907142541@qq.com>
2024-02-13 10:04:20 +0800

    新增ApplyRange

--- function_traits.hpp
+++ function_traits.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <tuple>
+#include <concepts>
+#include <cassert>
 
 namespace fustd {
 template<typename Func>
@@ -96,19 +98,50 @@ consteval bool MatchFunctionArguments() {
     return false;
 }
 
-template<typename ExpectedFunc, typename ActualFunc>
 // 匹配两个可调用对象的参数
 // 也就是ActualFunc的每个参数是否都可以隐式转换为ExpectedFunc对应的参数
+template<typename ExpectedFunc, typename ActualFunc>
 constexpr bool kIsArgumentsMatchableFunctions = MatchFunctionArguments<ExpectedFunc, ActualFunc>();
 
-template<typename ExpectedFunc, typename ActualFunc>
 // 匹配两个可调用对象的返回值
 // 也就是ActualFunc的返回类型是否可以隐式转换为ExpectedFunc的返回类型
+template<typename ExpectedFunc, typename ActualFunc>
 constexpr bool kIsReturnTypeMatchableFunctions = std::is_convertible_v<typename FunctionTraits<ExpectedFunc>::Return,
     typename FunctionTraits<ActualFunc>::Return>;
 
-template<typename ExpectedFunc, typename ActualFunc>
 // 匹配两个可调用对象
+template<typename ExpectedFunc, typename ActualFunc>
 constexpr bool kIsMatchableFunctions = kIsArgumentsMatchableFunctions<ExpectedFunc, ActualFunc>&&
     kIsReturnTypeMatchableFunctions< ExpectedFunc, ActualFunc>;
+
+
+namespace internal {
+template<typename, typename, typename, typename>
+struct ApplyRangeHelper;
+
+template<size_t... II, typename... Args, typename Func, typename ArgsRange>
+struct ApplyRangeHelper<std::index_sequence<II...>, std::tuple<Args...>, Func, ArgsRange>
+{
+    auto operator()(Func&& func, ArgsRange&& args) noexcept(std::is_nothrow_invocable_v<Func, Args...>)
+    {
+        static_assert(sizeof...(II) == sizeof...(Args));
+        return std::invoke(std::forward<Func>(func), (*reinterpret_cast<Args*>(args[II]))...);
+    }
+};
+}   // namespace internal
+
+// 将传入的参数容器展开, 并且对应转换为Args类型
+// 参数:
+//  func: 接受参数展开的回调函数
+//  args: 存储参数的容器, 必须是可随机访问的
+template<typename... Args, typename Func, std::ranges::random_access_range ArgsRange>
+auto ApplyRange(Func&& func, ArgsRange&& args) noexcept(std::is_nothrow_invocable_v<Func, Args...>)
+{
+    assert(sizeof...(Args) == std::size(args));
+    return internal::ApplyRangeHelper<
+        std::make_index_sequence<sizeof...(Args)>,
+        std::tuple<Args...>,
+        Func,
+        ArgsRange>{}(std::forward<Func>(func), std::forward<ArgsRange>(args));
+}
 }
